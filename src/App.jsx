2df86526e80e947d7e4dbec94adb2f748f471633
IAmGit@06 <aakashkumarsumanaksgi7@gmail.com>
2024-08-28 23:16:37 +0530

    smooth scrolling and everythin done except animatons

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ import Marquees from './components/Marquees';
 import Cards from './components/Cards';
 import Footer from './components/footer';
 import FooterLink from './components/FooterLink';
+import LocomotiveScroll from 'locomotive-scroll';
 
 export default function App(){
+    const locomotiveScroll = new LocomotiveScroll();
     return (
         <div className='bg-zinc-900 font-["satoshi"] text-white'>
             <div className=' w-full'>
