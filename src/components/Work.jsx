ee8d57c70976ec64e65f596be8bf6955e64cfd2c
IAmGit@06 <aakashkumarsumanaksgi7@gmail.com>
2024-08-29 09:29:05 +0530

    work animatin using framer motion

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useScroll} from "framer-motion";
 
 export default function Work(){
 
-    const images = [
-        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png", top: "56%", left: "49%", isActive: true},
-        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png", top: "59%", left: "47%", isActive: false},
-        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png", top: "64%", left: "53%", isActive: false},
-        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef092455ce2cf591e52d1_Rainfall.png", top: "60%", left: "51%", isActive: false},
-        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png", top: "55%", left: "55%", isActive: false},
-        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0af108a465002975acd_Showcase%20Websites%20(1).png", top: "51%", left: "62%", isActive: false}
-    ]
+    const [images, setImages] = useState([
+    
+        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png", top: "54%", left: "45%", isActive: false},
+        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png", top: "56%", left: "55%", isActive: false},
+        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png", top: "58%", left: "40%", isActive: false},
+        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef092455ce2cf591e52d1_Rainfall.png", top: "60%", left: "55%", isActive: false},
+        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png", top: "62%", left: "40%", isActive: false},
+        {url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0af108a465002975acd_Showcase%20Websites%20(1).png", top: "64%", left: "55%", isActive: false}
+    ])
 
+    const {scrollYProgress} = useScroll();
+    
+    scrollYProgress.on("change", (data)=>{
+        function imagesShow(arr){
+            setImages(prev => {
+                return prev.map((item, index)=>(
+                    arr.indexOf(index) === -1 ? {...item, isActive: false} : {...item, isActive: true}
+                ))
+            })
+        }
+        switch(Math.floor(data*100)){
+            case 0:
+                imagesShow([]);
+                break;
+            case 1:
+                imagesShow([0]);
+                break;
+            case 2:
+                imagesShow([0, 1]);
+                break;
+            case 3:
+                imagesShow([0, 1, 2]);
+                break;
+            case 4:
+                imagesShow([0, 1, 2, 3]);
+                break;
+            case 5:
+                imagesShow([0, 1, 2, 3, 4]);
+                break;
+            case 6:
+                imagesShow([0, 1, 2, 3, 4, 5]);
+                break;
+        }
+    })
     return (
         <div className='w-full'>
             <div className='relative max-w-screen-lg mx-auto'>
